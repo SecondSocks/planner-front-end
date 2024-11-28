86c8084b716edd9db9979f96de917b31fca54b76
SecondSocks <hitrovalubov267@gmail.com>
2024-11-28 22:44:51 +0700

    Project created. Base settings had written

---
+++ src/config/pages-url.config.ts
@@ -0,0 +1,12 @@
+class DASHBOARD {
+	private readonly root = '/i' // Путь админки
+
+	HOME = this.root
+	TASKS = `${this.root}/tasks`
+	HABITS = `${this.root}/habits`
+	TIMER = `${this.root}/timer`
+	TIME_BLOCKING = `${this.root}/time-blocking`
+	SETTINGS = `${this.root}/settings`
+}
+
+export const DASHBOARD_PAGES = new DASHBOARD()
---
+++ src/constants/seo.constants.ts
@@ -0,0 +1,2 @@
+export const NO_INDEX_PAGE = { robots: { index: false, follow: false } } // Означает, что страница не будет индексирована и не будет следить за ссылками
+export const SITE_NAME = 'Planner'
