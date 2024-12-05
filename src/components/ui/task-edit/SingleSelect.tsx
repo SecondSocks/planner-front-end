9843cb5cc850997deb3461365e2b5d4ae327b669
SecondSocks <159525526+SecondSocks@users.noreply.github.com>
2024-12-05 18:11:57 +0700

    Create hooks for tasks

---
+++ src/components/ui/task-edit/SingleSelect.tsx
@@ -0,0 +1,92 @@
+import cn from 'clsx'
+import { X } from 'lucide-react'
+
+import { useOutside } from '@/hooks/useOutside'
+
+import { Badge } from '../badge'
+
+export interface IOption {
+	label: string
+	value: string
+}
+
+interface ISingleSelect {
+	data: IOption[]
+	onChange: (value: string) => void
+	value: string
+	isColorSelect?: boolean
+}
+
+export function SigleSelect({
+	data,
+	onChange,
+	value,
+	isColorSelect
+}: ISingleSelect) {
+	const { isShow, setIsShow, ref } = useOutside(false)
+	const getValue = () => data.find(item => item.value === value)?.value
+
+	return (
+		<div
+			className={cn('relative min-w-36', {
+				'w-max': isColorSelect
+			})}
+			ref={ref}
+		>
+			<button
+				onClick={evt => {
+					evt.preventDefault()
+					setIsShow(!isShow)
+				}}
+			>
+				{getValue() ? (
+					<Badge
+						variant={value}
+						className='capitalize'
+						style={isColorSelect ? { backgroundColor: value } : {}}
+					>
+						{getValue()}
+					</Badge>
+				) : (
+					<Badge>Click for select</Badge>
+				)}
+			</button>
+			{value && (
+				<button
+					className='absolute top-0 right-0 opacity-30 hover:opacity-100 transition-opacity'
+					onClick={evt => {
+						evt.preventDefault()
+						onChange('')
+					}}
+				>
+					<X size={14} />
+				</button>
+			)}
+			{isShow && (
+				<div
+					className={cn(
+						'absolute w-full p-2.5 left-0 slide bg-sidebar z-10 shadow rounded-lg'
+					)}
+					style={{
+						top: 'calc(100% + .7rem)'
+					}}
+				>
+					{data.map(item => (
+						<button
+							key={item.value}
+							onClick={evt => {
+								evt.preventDefault()
+								onChange(item.value)
+								setIsShow(false)
+							}}
+							className='block mb-4 last:mb-0 capitalize rounded-lg'
+							style={isColorSelect ? { backgroundColor: item.value } : {}}
+						>
+							<Badge variant={item.value}>{item.label}</Badge>
+						</button>
+					))}
+				</div>
+			)}
+		</div>
+	)
+}
