632f6df79cb3f31a65c5a583d42a1adfb1a0e92e
SecondSocks <hitrovalubov267@gmail.com>
2026-08-29 12:03:18 +0700

    Fix: cookies political changes

--- src/services/auth-token.service.ts
+++ src/services/auth-token.service.ts
@@ -9,9 +9,7 @@ export const getAccessToken = () => {
 
 export const saveTokenStorage = (accessToken: string) => {
 	Cookies.set(EnumTokens.ACCESS_TOKEN, accessToken, {
-		domain: 'localhost',
-		sameSite: 'None',
-		secure: true,
+		sameSite: 'strict',
 		expires: 1
 	})
 }
