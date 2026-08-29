a09389f907b8162a772b3da8fcb05308fd4f17bf
SecondSocks <hitrovalubov267@gmail.com>
2026-08-29 12:30:54 +0700

    Fix: changes proxy to middleware (old next to new next middleware)

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,19 +5,19 @@ import { NextRequest, NextResponse } from 'next/server'
 import { DASHBOARD_PAGES } from './config/pages-url.config'
 import { EnumTokens } from './services/auth.service'
 
-export async function proxy(request: NextRequest) {
+export async function middleware(request: NextRequest) {
 	const { url, cookies } = request
 
-	const refreshToken = cookies.get(EnumTokens.REFRESH_TOKEN)?.value
+	const accessToken = cookies.get(EnumTokens.ACCESS_TOKEN)?.value
 
 	const isAuthPage = url.includes('/auth')
 
-	if (isAuthPage && refreshToken)
+	if (isAuthPage && accessToken)
 		return NextResponse.redirect(new URL(DASHBOARD_PAGES.HOME, url))
 
 	if (isAuthPage) return NextResponse.next()
 
-	if (!refreshToken) return NextResponse.redirect(new URL('/auth', url))
+	if (!accessToken) return NextResponse.redirect(new URL('/auth', url))
 
 	return NextResponse.next()
 }
